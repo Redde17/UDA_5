4827907b6eb1ae123768f471bb3455ca9058fe8a
Redde17 <redsmile797@gmail.com>
2021-03-10 11:24:46 +0100

    first commit

---
+++ main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Il php serve a far caricare il CSS senno' il browser non fa sempre richiesta al web server, quindi non si aggiorna-->
+    <style>
+        <?php include 'CSS/main.css'?>
+    </style>
+
+    <link rel="shortcut icon" type="image/x-icon" href="imgs/favicon.ico" />
+    <title>5M Libreria</title>
+</head>
+<body>
+    <div id="logoDiv">
+        <h1>5M</h1>
+    </div>
+    <div id="buttonDiv">
+        <h1>Lorem ipsum</h3>
+        <p>
+            dolor sit amet, ex usu vitae iudico. In partem ornatus mea. <br>
+            An modo vivendum his, ut deleniti vituperata has.
+        </p>
+        <button>qualcosa</button>
+    </div>
+</body>
+</html>
